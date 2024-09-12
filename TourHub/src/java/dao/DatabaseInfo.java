bfc2dda54a970d32f97f3e95db8113697ba49709
Nomsociuu <nomsociuu2004@gmail.com>
2024-09-12 20:24:31 +0700

    them database info

---
+++ TourHub/src/java/dao/DatabaseInfo.java
@@ -0,0 +1,12 @@
+package DAO;
+
+
+public interface DatabaseInfo {
+    public static String DRIVERNAME="com.microsoft.sqlserver.jdbc.SQLServerDriver";
+    public static String DBURL="jdbc:sqlserver://DESKTOP-EQJSVSA;databaseName=DoAnDatabase;encrypt=false;trustServerCertificate=false;loginTimeout=30";
+    public static String USERDB="sa";
+    public static String PASSDB="123";
+}
+
+//DESKTOP-EQJSVSA : Thien
+
