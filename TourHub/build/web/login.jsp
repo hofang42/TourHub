08eb0935c0369f1c09a5b664fe7cfb852746fe8b
Nomsociuu <nomsociuu2004@gmail.com>
2024-09-13 21:29:45 +0700

    restore

--- TourHub/build/web/login.jsp
+++ TourHub/build/web/login.jsp
@@ -1,4 +1,10 @@
+<%-- 
+    Document   : login.jsp
+    Created on : 3 May, 2024, 3:51:37 PM
+    Author     : HP
+--%>
 
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
     <head>
@@ -7,6 +13,7 @@
         <link rel="stylesheet" href="https://unpkg.com/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     </head>
     <body>
+        <!-- Login 11 - Bootstrap Brain Component -->
         <section class="py-3 py-md-5 py-xl-8">
             <div class="container">
                 <div class="row">
@@ -14,21 +21,15 @@
                         <div class="mb-5">
                             <h2 class="display-5 fw-bold text-center">Sign in</h2>
                             <p class="text-center m-0">Don't have an account? <a href="register.jsp">Sign up</a></p>
+
                         </div>
                     </div>
                 </div>
                 <div class="row justify-content-center">
                     <div class="col-12 col-lg-10 col-xl-8">
-                        <% String status = request.getParameter("status"); %>
-                        <% if ("resetSuccess".equals(status)) { %>
-                            <div class="alert alert-success text-center" role="alert">
-                                Change password successfully! You can login with your new password
-                            </div>
-                        <% } %>
-
                         <div class="row gy-5 justify-content-center">
                             <div class="col-12 col-lg-5">
-                                <form action="/login" method="post">
+                                <form action="#!">
                                     <div class="row gy-3 overflow-hidden">
                                         <div class="col-12">
                                             <div class="form-floating mb-3">
@@ -38,7 +39,7 @@
                                         </div>
                                         <div class="col-12">
                                             <div class="form-floating mb-3">
-                                                <input type="password" class="form-control border-0 border-bottom rounded-0" name="password" id="password" placeholder="Password" required>
+                                                <input type="password" class="form-control border-0 border-bottom rounded-0" name="password" id="password" value="" placeholder="Password" required>
                                                 <label for="password" class="form-label">Password</label>
                                             </div>
                                         </div>
@@ -46,37 +47,29 @@
                                             <div class="row justify-content-between">
                                                 <div class="col-6">
                                                     <div class="form-check">
-                                                        <input class="form-check-input" type="checkbox" name="remember_me" id="remember_me">
+                                                        <input class="form-check-input" type="checkbox" value="" name="remember_me" id="remember_me">
                                                         <label class="form-check-label text-secondary" for="remember_me">
                                                             Remember me
                                                         </label>
                                                     </div>
-                                                    <!-- Thông báo lỗi nếu có -->
                                                     <% if (request.getAttribute("error") != null) { %>
-                                                        <p style="color:red;"><%= request.getAttribute("error") %></p>
+                                                    <p style="color:red;"><%= request.getAttribute("error") %></p>
                                                     <% } %>
+
                                                 </div>
                                                 <div class="col-6">
                                                     <div class="text-end">
-                                                        <a href="forgotpassword.jsp" class="link-secondary text-decoration-none">Forgot password?</a>
+                                                        <a href="#!" class="link-secondary text-decoration-none">Forgot password?</a>
                                                     </div>
-
                                                 </div>
-                                                <% if (request.getAttribute("error") != null) { %>
-                                                <p style="color:red;"><%= request.getAttribute("error") %></p>
-                                                <% } %>
-
                                             </div>
-                                            
+                                        </div>
+                                        <div class="col-12">
+                                            <div class="d-grid">
+                                                <button class="btn btn-primary btn-lg" type="submit">Log in</button>
+                                            </div>
                                         </div>
                                     </div>
-
-                                    <div class="col-12">
-                                        <div class="d-grid">
-                                            <button class="btn btn-primary btn-lg" type="submit">Log in</button>
-                                        </div>
-                                    </div>
-
                                 </form>
                             </div>
                             <div class="col-12 col-lg-2 d-flex align-items-center justify-content-center gap-3 flex-lg-column">
@@ -87,26 +80,20 @@
                                 <div class="bg-dark w-100 d-lg-none" style="height: 1px; --bs-bg-opacity: .1;"></div>
                             </div>
                             <div class="col-12 col-lg-5 d-flex align-items-center">
-                                <div class="d-flex gap-3 flex-column w-100">
+                                <div class="d-flex gap-3 flex-column w-100 ">
                                     <a href="https://accounts.google.com/o/oauth2/auth?scope=email profile openid&redirect_uri=http://localhost:8080/Project_SWP/login&response_type=code&client_id=969957597236-ss9e1vcfd3f8p7p16joll4t4ipgubm44.apps.googleusercontent.com&approval_prompt=force" class="btn btn-lg btn-danger"> 
                                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-google" viewBox="0 0 16 16">
                                         <path d="M15.545 6.558a9.42 9.42 0 0 1 .139 1.626c0 2.434-.87 4.492-2.384 5.885h.002C11.978 15.292 10.158 16 8 16A8 8 0 1 1 8 0a7.689 7.689 0 0 1 5.352 2.082l-2.284 2.284A4.347 4.347 0 0 0 8 3.166c-2.087 0-3.86 1.408-4.492 3.304a4.792 4.792 0 0 0 0 3.063h.003c.635 1.893 2.405 3.301 4.492 3.301 1.078 0 2.004-.276 2.722-.764h-.003a3.702 3.702 0 0 0 1.599-2.431H8v-3.08h7.545z" />
                                         </svg>
                                         <span class="ms-2 fs-6">Sign in with Google</span>
                                     </a>
-
                                 </div>
-                            </form>
+                            </div>
                         </div>
-                        
                     </div>
                 </div>
             </div>
-
-        </div>
-    </section>
-
-</body>
-
-
+        </section>
+    </body>
+</html>
 
--- TourHub/src/java/dao/UserDB.java
+++ TourHub/src/java/dao/UserDB.java
@@ -1,11 +1,6 @@
 package DAO;
 
-import jakarta.servlet.http.Cookie;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
 import java.sql.*;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import model.User;
 import java.util.Date;
 
@@ -26,106 +21,12 @@ public class UserDB implements DatabaseInfo {
         return null;
     }
 
-    //Lấy all user ra
-    public User getUsers(String username, String password) {
-        User user = null;
-        String query = "Select username, password, userStatus, role, "
-                + "firstName, lastName, email, phone, address, createdAt"
-                + "from Users where username =? and password=? ";
-
-        try (Connection con = getConnect(); PreparedStatement stmt = con.prepareStatement(query)) {
-
-            stmt.setString(1, username);
-            stmt.setString(2, password);
-            ResultSet rs = stmt.executeQuery();
-
-            if (rs.next()) {
-                int id = rs.getInt(1);
-                username = rs.getString(2);
-                password = rs.getString(3);
-                String userStatus = rs.getString(4);
-                String role = rs.getString(5);
-                String firstName = rs.getString(6);
-                String lastName = rs.getString(7);
-                String email = rs.getString(8);
-                String phone = rs.getString(9);
-                String address = rs.getString(10);
-                Date createdAt = rs.getDate(11);
-                user = new User(id, username, password, userStatus, role, firstName, lastName, email, phone, address, createdAt);
-            }
-
-        } catch (Exception ex) {
-            Logger.getLogger(UserDB.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return user;
-    }
-
-    //Thêm user mới
-    public void insertUser(User user) {
-        String sql = "INSERT INTO Users (username, password, role, email) VALUES (?, ?, ?, ?)";
-
-        try (Connection con = getConnect(); PreparedStatement stmt = con.prepareStatement(sql)) {
-            stmt.setString(2, user.getUsername());
-            stmt.setString(3, user.getPassword());
-            stmt.setString(4, user.getRole());
-            stmt.setString(5, user.getEmail());
-            stmt.executeUpdate();
-        } catch (Exception ex) {
-            Logger.getLogger(UserDB.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    //Change password
-    public boolean updatePassword(int userId, String newPassword) {
-        String query = "UPDATE Users SET password=? WHERE UserId=?";
-
-        try (Connection con = getConnect(); PreparedStatement stmt = con.prepareStatement(query)) {
-
-            stmt.setString(1, newPassword);
-            stmt.setInt(2, userId);
-            int rowsUpdated = stmt.executeUpdate();
-            if (rowsUpdated == 0) {
-                throw new SQLException("Update failed, no rows affected.");
-            }
-            return true;
-        } catch (Exception ex) {
-            Logger.getLogger(UserDB.class.getName()).log(Level.SEVERE, null, ex);
-            return false; // Password update failed
-        }
-    }
-
-    //Edit profile function
-    public boolean updateUser(User user) {
-        boolean result = false;
-        String sql = "UPDATE Users SET "
-                + "firstName =?, lastName=?, email=?, phone=?, address=?, username=?"
-                + "from Users WHERE userId=?";
-        try (Connection conn = getConnect(); PreparedStatement stmt = conn.prepareStatement(sql)) {
-            stmt.setString(1, user.getFirstName());
-            stmt.setString(2, user.getLastName());
-            stmt.setString(3, user.getEmail());
-            stmt.setString(4, user.getPhone());
-            stmt.setString(5, user.getAddress());
-            stmt.setString(6, user.getUsername());
-            stmt.setInt(7, user.getUserId());
-
-            int rowsUpdated = stmt.executeUpdate();
-            if (rowsUpdated > 0) {
-                result = true;
-
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return result;
-    }
-
     public boolean registerUser(User user) {
         if (isUsernameExists(user.getUsername()) || isEmailExists(user.getEmail())) {
             return false; // Username or email already exists
         }
 
-        String sql = "INSERT INTO Users (username, password, firstName, lastName, phone, email, address, createdAt, userStatus, role) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO [User] (username, password, firstName, lastName, phone, email, address, createdAt, userStatus, role) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
         try (Connection conn = getConnect(); PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setString(1, user.getUsername());
             ps.setString(2, user.getPassword());
@@ -147,7 +48,7 @@ public class UserDB implements DatabaseInfo {
     }
 
     public void verifyUser(String email) {
-        String sql = "UPDATE Users SET verified = ? WHERE email = ?";
+        String sql = "UPDATE [User] SET verified = ? WHERE email = ?";
         try (Connection conn = getConnect(); PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setBoolean(1, true);
             ps.setString(2, email);
@@ -158,7 +59,7 @@ public class UserDB implements DatabaseInfo {
     }
 
     public User authenticate(String email, String password) {
-        String sql = "SELECT * FROM Users WHERE email = ? AND (password = ? OR password = '')"; // Handle no password for Google users
+        String sql = "SELECT * FROM [User] WHERE email = ? AND (password = ? OR password = '')"; // Handle no password for Google users
         try (Connection conn = getConnect(); PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setString(1, email);
             if (password != null) {
@@ -172,15 +73,14 @@ public class UserDB implements DatabaseInfo {
                         rs.getInt("userId"),
                         rs.getString("username"),
                         rs.getString("password"),
-                        rs.getString("userStatus"), // Fetch userStatus
-                        rs.getString("role"),
                         rs.getString("firstName"),
                         rs.getString("lastName"),
                         rs.getString("phone"),
                         rs.getString("email"),
                         rs.getString("address"),
-                        rs.getTimestamp("createdAt")
-                // Fetch role
+                        rs.getTimestamp("createdAt"),
+                        rs.getString("userStatus"), // Fetch userStatus
+                        rs.getString("role") // Fetch role
                 );
             }
         } catch (SQLException e) {
@@ -189,32 +89,8 @@ public class UserDB implements DatabaseInfo {
         return null;
     }
 
-    public User getUserFromSession(HttpSession session, HttpServletRequest request) {
-        String userName = (String) session.getAttribute("user");
-        String pass = (String) session.getAttribute("pass");
-
-        if (userName == null || pass == null) {
-            Cookie[] cookies = request.getCookies();
-            if (cookies != null) {
-                for (Cookie cookie : cookies) {
-                    if (cookie.getName().equals("user")) {
-                        userName = cookie.getValue();
-                    }
-                    if (cookie.getName().equals("pass")) {
-                        pass = cookie.getValue();
-                    }
-                }
-            }
-        }
-
-        if (userName != null && pass != null) {
-            return getUsers(userName, pass);
-        }
-        return null;
-    }
-
     public boolean isUsernameExists(String username) {
-        String sql = "SELECT COUNT(*) FROM Users WHERE username = ?";
+        String sql = "SELECT COUNT(*) FROM [User] WHERE username = ?";
         try (Connection conn = getConnect(); PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setString(1, username);
             ResultSet rs = ps.executeQuery();
@@ -228,7 +104,7 @@ public class UserDB implements DatabaseInfo {
     }
 
     public boolean isEmailExists(String email) {
-        String sql = "SELECT COUNT(*) FROM Users WHERE email = ?";
+        String sql = "SELECT COUNT(*) FROM [User] WHERE email = ?";
         try (Connection conn = getConnect(); PreparedStatement ps = conn.prepareStatement(sql)) {
             ps.setString(1, email);
             ResultSet rs = ps.executeQuery();
@@ -239,44 +115,5 @@ public class UserDB implements DatabaseInfo {
             e.printStackTrace();
         }
         return false;
-    }
-
-    //Kiem tra email co trong database hay la khong
-    public boolean checkEmailExists(String email) {
-        boolean exists = false;
-        Connection conn = null;
-        PreparedStatement ps = null;
-        ResultSet rs = null;
-
-        String query = "SELECT email FROM Users WHERE email = ?";
-        try {
-            conn = getConnect();
-            if (conn != null) {
-                ps = conn.prepareStatement(query);
-                ps.setString(1, email);
-                rs = ps.executeQuery();
-
-                if (rs.next()) {
-                    exists = true;  // Email co trong database
-                }
-            }
-        } catch (Exception e) {
-            System.out.println(e);
-        } finally {
-            try {
-                if (rs != null) {
-                    rs.close();
-                }
-                if (ps != null) {
-                    ps.close();
-                }
-                if (conn != null) {
-                    conn.close();
-                }
-            } catch (Exception e) {
-                System.out.println(e);
-            }
-        }
-        return exists;
     }
 }
